2ce623e434c4886d5245bf6d8917151869a84cdd
Steven Fisher <tewha@me.com>
2013-11-19 12:01:21 -0800

    Baseline add.

---
+++ FlipTest/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  FlipTest
+//
+//  Created by Steven Fisher on 11/19/2013.
+//  Copyright (c) 2013 Steven Fisher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ FlipTest/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  FlipTest
+//
+//  Created by Steven Fisher on 11/19/2013.
+//  Copyright (c) 2013 Steven Fisher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
